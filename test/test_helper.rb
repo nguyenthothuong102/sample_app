f070033e629686fdb7ff5eb2ea06df8f56afb106
nguyenthothuong102 <thuongmonster.slna37@gmail.com>
2019-10-31 09:06:52 +0700

    Chapter 8 - Basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,10 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   fixtures :all
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
